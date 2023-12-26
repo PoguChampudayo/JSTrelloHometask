42f34b496cfd5acbc39b263248efc9a02890d9ce
Aleksey Gordeyev <agordeyev94@gmail.com>
2023-12-26 21:22:06 +0500

    1

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
-import Controller from "../js/Controller";
+import Controller from "./Controller";
 import { saveToStorage, loadFromStorage } from "./localStorage";
+import Card from "./Card";
 
 const controller = new Controller();
 controller.init();
